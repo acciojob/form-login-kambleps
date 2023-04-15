d07762382c7538cd7fff5352a5ccddcd497f73b9
kambleps <125243027+kambleps@users.noreply.github.com>
2023-04-15 21:34:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+let fname = document.querySelector("input[name='fname']").value;
+    let lname = document.querySelector("input[name='lname']").value
 
+    alert(fname + " " + lname);
 }
